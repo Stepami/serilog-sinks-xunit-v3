1e7a288a775adfcc859f964095f52b22f2874c76
Stepami <stepami@mail.ru>
2025-09-22 17:24:28 +0300

    xmldocs

--- Serilog.Sinks.XUnit3/XUnit3TestOutputSink.cs
+++ Serilog.Sinks.XUnit3/XUnit3TestOutputSink.cs
@@ -5,6 +5,12 @@ using Xunit;
 
 namespace Serilog.Sinks.XUnit3;
 
+/// <summary>
+///     The sink for logging through Serilog to xUnit.v3 test output.
+///     The instance of this class needs to be injected to DI.
+/// </summary>
+/// <param name="outputTemplate">Format of the log message</param>
+/// <param name="formatProvider">Format source</param>
 public sealed class XUnit3TestOutputSink(
     string outputTemplate = XUnit3TestOutputSink.DefaultTemplate,
     IFormatProvider? formatProvider = null) : ILogEventSink
@@ -13,8 +19,14 @@ public sealed class XUnit3TestOutputSink(
 
     private readonly MessageTemplateTextFormatter _messageTemplateTextFormatter = new(outputTemplate, formatProvider);
 
+    /// <summary>
+    ///     Reference to xUnit.v3 <see cref="ITestOutputHelper"/>.
+    ///     Needs to be set through DI in cases the sink runs on background thread.
+    ///     By default, backed by <c>TestContext.Current.TestOutputHelper</c> during <see cref="XUnit3TestOutputSink.Emit"/> call.
+    /// </summary>
     public ITestOutputHelper? TestOutputHelper { get; set; }
 
+    /// <inheritdoc cref="ILogEventSink.Emit"/>
     public void Emit(LogEvent logEvent)
     {
         ArgumentNullException.ThrowIfNull(logEvent);
